031942f8ee8e4d6e98c3a660da2721047b2fa9ab
seanperry <seanep94@gmail.com>
2024-02-03 21:07:49 -0800

    The tutorial didnt mention to add the Script tags into the HTML.....

--- blog/blog/myblog/views.py
+++ blog/blog/myblog/views.py
@@ -7,12 +7,11 @@ class HomeView(ListView):
     # template_name = "blog/index.html"
     context_object_name = "posts"
     paginate_by = 10
-
     def get_template_names(self):
-        is_htmx = self.request.headers.get('HX-Request') == True
+        is_htmx = self.request.headers.get('HX-Request') != None
         print(is_htmx, self.request.headers.get('HX-Request'))
         if is_htmx:
             print('succ')
-            return "../components/post-list-elements.html"
+            return "components/post-list-elements.html"
         print('fuc a ')
         return "blog/index.html"
