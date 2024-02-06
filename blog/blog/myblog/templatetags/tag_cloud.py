81f41af5a947fd21db59fac91e69b1dd727822d5
seanperry <seanep94@gmail.com>
2024-02-05 20:20:27 -0800

    Added the Tags to display on the website

--- blog/blog/myblog/templatetags/tag_cloud.py
+++ blog/blog/myblog/templatetags/tag_cloud.py
@@ -1,2 +1,13 @@
 from django import template
-from taggit.models 
+from taggit.models import Tag
+
+
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("components/tag-cloud.html")
+def sidebar_tag_cloud():
+    x = Tag.objects.all()
+    return {"tags": x}
